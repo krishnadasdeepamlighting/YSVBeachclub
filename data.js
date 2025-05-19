81f9b96c68a7e8b6c948aca532366dbfec5f2550
krishnadasdeepamlighting <krishnadas.deepamlighting@gmail.com>
2025-05-19 15:08:05 +0530

    imgs

--- data.js
+++ data.js
@@ -36,6 +36,12 @@ var APP_DATA = {
           "pitch": 0.07672308328762334,
           "rotation": 0,
           "target": "2-vraycam008"
+        },
+        {
+          "yaw": -0.8116296237311396,
+          "pitch": 0.04739561869276798,
+          "rotation": 0,
+          "target": "3-vraycam004"
         }
       ],
       "infoHotspots": []
@@ -74,8 +80,20 @@ var APP_DATA = {
         {
           "yaw": -1.1522715178466747,
           "pitch": 0.20077700396369735,
-          "rotation": 0,
+          "rotation": 0.7853981633974483,
           "target": "2-vraycam008"
+        },
+        {
+          "yaw": -0.35637893936877596,
+          "pitch": -0.02162138310465167,
+          "rotation": 0,
+          "target": "4-vraycam007"
+        },
+        {
+          "yaw": -1.6573401614798726,
+          "pitch": 0.013259678546356568,
+          "rotation": 0,
+          "target": "3-vraycam004"
         }
       ],
       "infoHotspots": []
@@ -110,6 +128,110 @@ var APP_DATA = {
           "pitch": 0.30716265706745105,
           "rotation": 0,
           "target": "1-vraycam003"
+        },
+        {
+          "yaw": 2.296348928262339,
+          "pitch": 0.1428085494139193,
+          "rotation": 0,
+          "target": "0-vraycam001"
+        },
+        {
+          "yaw": -1.53696580491774,
+          "pitch": 0.03064773808849708,
+          "rotation": 0,
+          "target": "3-vraycam004"
+        },
+        {
+          "yaw": 0.36484087468013016,
+          "pitch": -0.06215245519313051,
+          "rotation": 0,
+          "target": "4-vraycam007"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-vraycam004",
+      "name": "VRayCam004",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5856993514813027,
+          "pitch": 0.11434199700030945,
+          "rotation": 0,
+          "target": "2-vraycam008"
+        },
+        {
+          "yaw": 1.7088185985579782,
+          "pitch": 0.0917660689113049,
+          "rotation": 0,
+          "target": "1-vraycam003"
+        },
+        {
+          "yaw": 2.1039875001167676,
+          "pitch": 0.053863724614984676,
+          "rotation": 0,
+          "target": "0-vraycam001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-vraycam007",
+      "name": "VRayCam007",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2500382423079834,
+          "pitch": 0.16338937078218407,
+          "rotation": 0,
+          "target": "1-vraycam003"
+        },
+        {
+          "yaw": -1.54161305342115,
+          "pitch": 0.128088600878824,
+          "rotation": 0,
+          "target": "3-vraycam004"
         }
       ],
       "infoHotspots": []
